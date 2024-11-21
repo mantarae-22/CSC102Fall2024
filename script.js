9ce424b712165ba78f134c646fbaba16f9f98e99
Rae Crusoe <113061355+mantarae-22@users.noreply.github.com>
2024-11-21 12:13:15 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -31,3 +31,49 @@ function validateForm(){
     // we don't want this page to submit to a server/another page, so prevent that from happening
     return false;
 }
+
+
+/* sound code */
+// this function will add sound to our page
+function addSound(){
+    // create a new audio HTML element
+    var soundElem = document.createElement("audio");
+    // nickname/shortcut to the play button
+    var btnPlay = document.getElementById("btnPlay");
+    // nickname/shortcut to the pause button
+    var btnPause = document.getElementById("btnPause");
+
+    // set up the id="audioSound" attribute on our audio element
+    soundElem.setAttribute("id", "audioSound");
+
+    // set up the src="us-lab-background.mp3"
+    // src = source = the file name
+    soundElem.setAttribute("src", "us-lab-background.mp3");
+
+    // add the audio element that we just created to the HTML page
+    document.body.appendChild(soundElem);
+
+    // un-hide the play and pause buttons
+    btnPlay.hidden = false;
+    btnPause.hidden = false;
+}
+
+// set up the function to play the sound
+function playSound(){
+    // set up a nickname/shortcut to the audio element that we created in the addSound() function
+    var audio = document.getElementById("audioSound");
+    // start playing the audio
+    audio.play();
+    // log the playing started to the console because of sound issues on this computer
+    console.log("sound playing");
+}
+
+// set up the function to pause the playing of the sound
+function pauseSound(){
+    // set up a nickname/shortcut to the audio element that we created in the addSound() function
+    var audio = document.getElementById("audioSound");
+    // pause the playing of the sound
+    audio.pause();
+    // log the playing paused to the console because of sound issues on this computer
+    console.log("sound paused");
+}
